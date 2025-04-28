fee0cc183bbec1210df3051180c93e3bd651c4e6
sethblocks <56612770+sethblocks@users.noreply.github.com>
2025-04-27 22:32:15 -0700

    Add basic AI, expand map
    TODO: Add more sheep, detect nearest to attack

--- fps_indicator_test.gd
+++ fps_indicator_test.gd
@@ -2,22 +2,13 @@ extends Label
 
 var graphicslog = ""
 
-func _ready() -> void:
-	OS.alert("This is a super early control/graphics test demo, your FPS will be tracked and saved.", "GPU")
-	graphicslog += "Using GPU:" + RenderingServer.get_rendering_device().get_device_name()
-	graphicslog += "\n\nOS: " + OS.get_name()
 	
 func _notification(what):
 	if what == NOTIFICATION_WM_CLOSE_REQUEST:
 		print("bye")
 		var file = FileAccess.open("user://graphics.log", FileAccess.WRITE)
 		file.store_string(graphicslog)
-		OS.alert("Showing Graphics Log")
-		OS.shell_show_in_file_manager(file.get_path_absolute())
-var timer = 0.0
+		
+
 func _process(delta):
-	timer+=delta
 	self.text = ("FPS:" + str(Engine.get_frames_per_second()))
-	if timer > 0.5:
-		timer = 0.0
-		graphicslog += "\n" + str(Engine.get_frames_per_second())
--- npsheep.gd
+++ npsheep.gd
@@ -1,6 +1,18 @@
 extends RigidBody3D
 var explosion = preload("res://assets/explode.tscn").instantiate()
 func _process(delta: float) -> void:
+	var target = get_node("../RigidBody3D")
+	var target_dir = (target.global_transform.origin - global_transform.origin).normalized()
+	var current_dir = global_transform.basis.z.normalized()
+	
+	var rotation_axis = current_dir.cross(target_dir).normalized()
+	var angle_difference = acos(current_dir.dot(target_dir))
+	
+	var rotation_speed = 10.0
+	
+	angular_velocity = rotation_axis * angle_difference * rotation_speed
+	
+	self.apply_force(global_transform.basis * Vector3(0,0,75.0))
 	if abs(self.rotation_degrees.z) > 70:
 		explosion.global_position = self.global_position
 		get_parent().add_child(explosion)
